713ad44887570534977d0e33bf0c9c510d26fa43
ssyoni <jsy950601@gmail.com>
2021-06-07 22:55:32 +0900

    add orders api test case

--- src/test/java/noums/study/pizzahouse/PizzahouseApplicationTests.java
+++ src/test/java/noums/study/pizzahouse/PizzahouseApplicationTests.java
@@ -1,13 +1,41 @@
 package noums.study.pizzahouse;
 
+import noums.study.pizzahouse.controller.OrderController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import javax.print.attribute.standard.Media;
+import java.awt.*;
+import java.nio.charset.Charset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest //(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class PizzahouseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    @Test
+    public void postUrlTest() throws Exception {
+        this.mockMvc.perform(post("/orders").contentType(contentType)
+                .content("{\"menu\" : \"BULGOGI_PIZZA\"}")).andExpect(status().isOk()).andDo(print());
+    }
 
 }
